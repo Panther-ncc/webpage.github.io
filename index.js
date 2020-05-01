923eb6d40f672270c4b6c9cc2d72341f9b66839f
Panther-ncc <55699348+Panther-ncc@users.noreply.github.com>
2020-05-01 12:50:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+alert("hello user");
+var username=prompt("please login with ur gmail, Enter ur username ");
+var pass=prompt("enter your password");
+console.log(username);
+console.log(pass);
+alert("Thank u for login");
+document.write("welcome ",username);
+document.getElementByid('Apples').innerHTML='papaya';
